9cabf755a22eaf579aa9a9fd4c40eea7f0a606c1
samuel <tshisamuel47@gmail.com>
2024-09-10 23:59:24 +0100

    pin changed 2

--- src/pin_config.h
+++ src/pin_config.h
@@ -3,34 +3,34 @@
 #define PIN_CONFIG_H
 
 // I2S Amplifier (MAX98357AEW)
-#define I2S_SD_MODE_PIN GPIO_NUM_5
-#define I2S_BCLK_PIN GPIO_NUM_7
-#define I2S_DIN_PIN GPIO_NUM_6
+#define I2S_SD_MODE_PIN 5
+#define I2S_BCLK_PIN 7
+#define I2S_DIN_PIN 6
 
 // SD Card Module
 //Form real device
-#define SD_MISO_PIN GPIO_NUM_42    // DATA0/MISO
-#define SD_CLK_PIN GPIO_NUM_41     // CLK
+#define SD_MISO_PIN 42    // DATA0/MISO
+#define SD_CLK_PIN 41     // CLK
 
 
-#define SD_MOSI_PIN GPIO_NUM_39    // CMD/MOSI
-#define SD_CS_PIN GPIO_NUM_38      // CS/DATA3
+#define SD_MOSI_PIN 39    // CMD/MOSI
+#define SD_CS_PIN 38      // CS/DATA3
 
 // Microphone Module (MAX9814ETD)
-#define MIC_OUT_PIN GPIO_NUM_4     // MICOUT
+#define MIC_OUT_PIN 4     // MICOUT
 
 //Form real device
-#define MIC_GAIN_PIN GPIO_NUM_47   // GAINMIC
-#define MIC_AR_PIN GPIO_NUM_48     // A/R
+#define MIC_GAIN_PIN 47   // GAINMIC
+#define MIC_AR_PIN 48     // A/R
 
 
 
 //LEDs
-#define LED1_PIN GPIO_NUM_18
-#define LED2_PIN GPIO_NUM_8
+#define LED1_PIN 18
+#define LED2_PIN 8
 
 // Buttons (Pulled up)
-#define BUTTON1_PIN GPIO_NUM_16
-#define BUTTON2_PIN GPIO_NUM_17
+#define BUTTON1_PIN 16
+#define BUTTON2_PIN 17
 
 #endif // PIN_CONFIG_H
